a7bebe88a8fe01673da7078899cb46c07298e28d
Abdul Kholik <akl859349@gmail.com>
2025-03-03 21:04:38 +0700

    Add API routes for categories, reviews, and galleries

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ReviewController;
+use App\Http\Controllers\GalleryController;
+use App\Http\Controllers\CategoryController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +20,7 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::apiResource('categories', CategoryController::class);
+Route::apiResource('reviews', ReviewController::class);
+Route::apiResource('galleries', GalleryController::class);
